6d8c34a6999b6cf4e279aef59572146c9de0b48f
Chris Havlin <chris.havlin@gmail.com>
2020-08-18 15:21:09 -0700

    initial formatting working

--- test_vtk_tools.py
+++ test_vtk_tools.py
@@ -17,3 +17,8 @@ b = sf(72,[2,2,2])
 c = sf(72,[2,2,2],vtk_version='8.0.1')
 print([b.node_order_hash[18],c.node_order_hash[19]])
 print([b.node_order_hash[19],c.node_order_hash[18]])
+
+
+b.format_shape_functions()
+b.format_shape_functions(sum=False)
+b.format_shape_functions(fmt='yt')
--- vtk_tools/shapefunctions.py
+++ vtk_tools/shapefunctions.py
@@ -28,7 +28,7 @@ class sf(object):
             setattr(self,attr_name,getattr(self.cell,meth)())
 
         self.node_order_hash = self._build_point_hash()
-        # self.shape_functions = self._build_shape_funcs()        
+        self.shape_functions = self._build_shape_funcs()        
         
     def _build_point_hash(self):
         # returns a dict with keys-value pairs of vtk_node_number : ijk node number 
@@ -41,9 +41,8 @@ class sf(object):
         #     for j in range(dim1+1):
         #         for k in range(dim2+1):
         #             pts.append(self.cell.PointIndexFromIJK(i,j,k))
-                
-        ijkvals = self._get_ijk_permuatations()
-        for ijk in ijkvals:
+                        
+        for ijk in self._get_ijk_permuatations():
             if hasattr(self.cell,'PointIndexFromIJK'):
                 pts.append(self.cell.PointIndexFromIJK(*ijk))
             else:
@@ -60,20 +59,34 @@ class sf(object):
         return node_hash
                    
     def _build_shape_funcs(self):
-        
-        dim0,dim1,dim2 = self.element_order
+        # builds sympy expressions for shape function evalulation 
         
         shape_funcs = []
         x=sy.symbols('x')
         y=sy.symbols('y')
-        z=sy.symbols('z')
-        for z_i in range(dim0+1):
-            for y_i in range(dim1+1):
-                for x_i in range(dim2+1):
-                    LP1 = LagrangPoly(x,dim0,x_i,[-1,0,1])
-                    LP2 = LagrangPoly(y,dim1,y_i,[-1,0,1])
-                    LP3 = LagrangPoly(z,dim2,z_i,[-1,0,1])
-                    shape_funcs.append(sy.simplify(LP1 * LP2 * LP3))
+        z=sy.symbols('z')                
+        pos_i,pos_j,pos_k = self._get_ijk_positions(*self.element_order)
+                
+        for ijk in self._get_ijk_permuatations():
+            LPi = LagrangPoly(x,self.element_order[0],ijk[0],pos_i)
+            LPj = 1
+            LPk = 1 
+            
+            if len(ijk) > 1: 
+                LPj= LagrangPoly(y,self.element_order[1],ijk[1],pos_j)                
+            if len(ijk) > 2: 
+                LPk = LagrangPoly(z,self.element_order[2],ijk[2],pos_k)
+                
+            shape_funcs.append(sy.simplify(LPi * LPj * LPk))
+            
+        # dim0,dim1,dim2 = self.element_order            
+        # for z_i in range(dim0+1):
+        #     for y_i in range(dim1+1):
+        #         for x_i in range(dim2+1):
+        #             LP1 = LagrangPoly(x,dim0,x_i,[-1,0,1])
+        #             LP2 = LagrangPoly(y,dim1,y_i,[-1,0,1])
+        #             LP3 = LagrangPoly(z,dim2,z_i,[-1,0,1])
+        #             shape_funcs.append(sy.simplify(LP1 * LP2 * LP3))
         return shape_funcs 
         
     def _get_ijk_permuatations(self):
@@ -98,6 +111,58 @@ class sf(object):
                 ig = ig.ravel(order='C')
                 jg = jg.ravel(order='C')
                 return np.column_stack((ig,jg,kg)).tolist()
+    
+    def _get_ijk_positions(self,order_i,order_j=None,order_k=None):
+        # builds list of positions for each coordinate in parent element 
+        pos_i = np.linspace(-1,1,order_i+1).tolist()
+        pos_j = [] 
+        pos_k = [] 
+
+        if order_j is not None: 
+            pos_j = np.linspace(-1,1,order_j+1).tolist()
+        
+        if order_k is not None:
+            pos_k = np.linspace(-1,1,order_k+1).tolist()
+            
+        return pos_i,pos_j,pos_k
                 
+    
     def _get_cell_id_from_ijk(self,i,j=None,k=None):        
         raise NotImplementedError("Cell type does not hav an ijk mapping yet.")
+
+    def format_shape_functions(self,outputfile = 'shapefunctions.txt',sum=True, fmt = None, progress = True):                        
+                        
+        def _applyformatting(shape_func,Lvalue):
+            # substitution rules, convert to string 
+            x, y, z = sy.symbols('x,y,z')
+            vl = sy.symbols('v'+str(Lvalue))
+            if fmt == 'yt':
+                # x,y,z are coord[0],coord[1],coord[2] respectively
+                for c in [[x,0],[y,1],[z,2]]:
+                    shape_func = shape_func.replace(c[0],sy.Symbol(f'coord[{c[1]}]'))  
+                    vl = sy.symbols('values['+str(Lvalue)+']')
+                    
+                shape_func = vl * shape_func
+                shape_func = str(shape_func)
+                spaces = ' '.join([' ']*4)
+                if sum:
+                    sum_str = ' +'
+                else:
+                    sum_str = ''
+                shape_func = spaces + shape_func + sum_str + '\n'
+                
+            else:
+                shape_func = str(shape_func)
+                
+            return shape_func        
+        
+        with open(outputfile,'w') as fhandle:
+                
+            for Lnum,sf in enumerate(self.shape_functions):
+                
+                sf = _applyformatting(sf,Lnum)
+                if progress:
+                    print(sf)                 
+                fhandle.write(sf)
+            
+        pass 
