973b101a487bc6eca8784f463fbccb73fca08749
Chris Havlin <chris.havlin@gmail.com>
2020-08-19 10:47:35 -0700

    yaml support working

--- test_vtk_tools.py
+++ test_vtk_tools.py
@@ -1,24 +1,32 @@
 from vtk_tools.vtk_tools import init_vtk_cell
+import vtk_tools.shapefunctions as sfs
+import vtk_tools.yaml_support as yams
 
 v_cell,_ = init_vtk_cell(28)
 print(type(v_cell))
 
-from vtk_tools.shapefunctions import sf
-b = sf(72,[2,2,2])
+b = sfs.sf(72,[2,2,2])
 print(b.node_order_hash)
 
-b = sf(72,1)
+b = sfs.sf(72,1)
 print(b.node_order_hash)
 
-b = sf(72,[2,1,2])
+b = sfs.sf(72,[2,1,2])
 print(b.node_order_hash)
 
-b = sf(72,[2,2,2])
-c = sf(72,[2,2,2],vtk_version='8.0.1')
+b = sfs.sf(72,[2,2,2])
+c = sfs.sf(72,[2,2,2],vtk_version='8.0.1')
 print([b.node_order_hash[18],c.node_order_hash[19]])
 print([b.node_order_hash[19],c.node_order_hash[18]])
 
+b = sfs.sf(72,[1,1,1])
+b.write_yaml('testyaml.yaml','w','test_hex8','hex8')
 
-b.format_shape_functions()
-b.format_shape_functions(sum=False)
-b.format_shape_functions(fmt='yt')
+b = sfs.sf(72,[2,2,2])
+b.write_yaml('testyaml.yaml','a','test_hex27','hex27')
+
+yt_h = yams.yt_helper()
+print("yt mesh types in autogenerator: ")
+print(yt_h.available_mesh_types)
+print("hex8 shapefunctions:")
+print(yt_h.mesh_types['Hex8']['shape_functions'])
--- vtk_tools/shapefunctions.py
+++ vtk_tools/shapefunctions.py
@@ -1,23 +1,30 @@
 import sympy as sy 
 from .lagrange import LagrangPoly
 from .vtk_tools import init_vtk_cell,require_vtk_min_version
+import yaml 
+from . import yaml_support 
 import numpy as np 
+import os 
 
 require_vtk_min_version()
 
 class sf(object):
     def __init__(self,vtk_type,element_order=1,vtk_version='9.0.1'):
         self.cell, self.cell_type = init_vtk_cell(vtk_type)        
-        self.n_dims = self.cell.GetCellDimension()
+        self.num_dims = self.cell.GetCellDimension()
         
         self.vtk_create_version=vtk_version
         self.vtk_create_major_version=int(vtk_version.split('.')[0])
         self.vtk_create_minor_version=int(vtk_version.split('.')[1])
         
-        # make element order a list of length n_dims if it's not a list 
+        # make element order a list of length num_dims if it's not a list 
         if isinstance(element_order,int):
-            element_order = [element_order] * self.n_dims
+            element_order = [element_order] * self.num_dims
         self.element_order = element_order 
+        
+        # calculate number of vertices 
+        els = np.array(self.element_order) + 1 
+        self.num_vertices = int(np.prod(els))
         
         if hasattr(self.cell,'SetOrder'):
             self.cell.SetOrder(*self.element_order)
@@ -33,14 +40,7 @@ class sf(object):
     def _build_point_hash(self):
         # returns a dict with keys-value pairs of vtk_node_number : ijk node number 
         pts = []
-        els = np.array(self.element_order) + 1 
-        node_nums = range(0,np.prod(els))
-
-        # dim0,dim1,dim2 = self.element_order                
-        # for i in range(dim0+1):
-        #     for j in range(dim1+1):
-        #         for k in range(dim2+1):
-        #             pts.append(self.cell.PointIndexFromIJK(i,j,k))
+        node_nums = range(0,self.num_vertices)
                         
         for ijk in self._get_ijk_permuatations():
             if hasattr(self.cell,'PointIndexFromIJK'):
@@ -79,27 +79,19 @@ class sf(object):
                 
             shape_funcs.append(sy.simplify(LPi * LPj * LPk))
             
-        # dim0,dim1,dim2 = self.element_order            
-        # for z_i in range(dim0+1):
-        #     for y_i in range(dim1+1):
-        #         for x_i in range(dim2+1):
-        #             LP1 = LagrangPoly(x,dim0,x_i,[-1,0,1])
-        #             LP2 = LagrangPoly(y,dim1,y_i,[-1,0,1])
-        #             LP3 = LagrangPoly(z,dim2,z_i,[-1,0,1])
-        #             shape_funcs.append(sy.simplify(LP1 * LP2 * LP3))
         return shape_funcs 
         
     def _get_ijk_permuatations(self):
         # returns a list of ijk values for looping over element nodes in 1d, 2d, 3d.     
         
-        if self.n_dims == 1: 
+        if self.num_dims == 1: 
             ivals = range(0,self.element_order[0]+1)
             return [[ival] for ival in ivals]
-        elif self.n_dims > 1: 
+        elif self.num_dims > 1: 
             ivals = range(self.element_order[0]+1)
             jvals = range(self.element_order[1]+1)
             
-            if self.n_dims == 2: 
+            if self.num_dims == 2: 
                 ig,jg = np.meshgrid(ivals,jvals,indexing='ij')
                 ig = ig.ravel(order='C')
                 jg = jg.ravel(order='C')
@@ -130,39 +122,123 @@ class sf(object):
     def _get_cell_id_from_ijk(self,i,j=None,k=None):        
         raise NotImplementedError("Cell type does not hav an ijk mapping yet.")
 
-    def format_shape_functions(self,outputfile = 'shapefunctions.txt',sum=True, fmt = None, progress = True):                        
-                        
-        def _applyformatting(shape_func,Lvalue):
-            # substitution rules, convert to string 
-            x, y, z = sy.symbols('x,y,z')
-            vl = sy.symbols('v'+str(Lvalue))
-            if fmt == 'yt':
-                # x,y,z are coord[0],coord[1],coord[2] respectively
-                for c in [[x,0],[y,1],[z,2]]:
-                    shape_func = shape_func.replace(c[0],sy.Symbol(f'coord[{c[1]}]'))  
-                    vl = sy.symbols('values['+str(Lvalue)+']')
-                    
-                shape_func = vl * shape_func
-                shape_func = str(shape_func)
-                spaces = ' '.join([' ']*4)
-                if sum:
-                    sum_str = ' +'
-                else:
-                    sum_str = ''
-                shape_func = spaces + shape_func + sum_str + '\n'
-                
-            else:
-                shape_func = str(shape_func)
-                
-            return shape_func        
+    def write_yaml(self,file,write_mode,mesh_name,mesh_type=None,fmt='yt'):
+        """writes element info, including shape functions to a yaml file. 
+
+        Parameters
+        ----------
+        file : str
+            filename to write to 
+        write_mode : str
+            file write mode, 'w' or 'a'
+        mesh_name : str
+            what to name this element entry.
+        mesh_type : str
+            mesh type, required if using fmt='yt'.
+        fmt : str
+            the yaml format to use. Can be 'yt' or None (the default is 'yt').
+
+        Examples
+        --------
         
-        with open(outputfile,'w') as fhandle:
+        to write two elements to a new yaml file with 'yt' formatting: 
+        
+        import vtk_tools.shapefunctions as sfs
+        el_1 = sfs.sf(72,[1,1,1])
+        el_1.write_yaml('testyaml.yaml','w','test_hex8','hex8')
+
+        el_2 = sfs.sf(72,[2,2,2])
+        el_2.write_yaml('testyaml.yaml','a','test_hex27','hex27')
+
+        """
                 
-            for Lnum,sf in enumerate(self.shape_functions):
-                
-                sf = _applyformatting(sf,Lnum)
-                if progress:
-                    print(sf)                 
-                fhandle.write(sf)
+        yaml_dict = {}
+        if fmt =='yt':  
+            if mesh_type is None:
+                raise ValueError("mesh_type must be provided for yt formatting.")      
+            yaml_dict[mesh_name] = sf_yt_formatter(
+                                                   mesh_type,
+                                                   self.num_dims,
+                                                   self.num_vertices,
+                                                   self.shape_functions
+                                                   ).get_dict()   
+        else:                     
+            formatter = sf_formatter()
+            formatter.set_shape_functions(self.shape_functions)
+            yaml_dict[mesh_name] = formatter.get_dict()
+                        
+        with open(file,write_mode) as yaml_stream:
+            yaml.dump(yaml_dict,yaml_stream)    
+    
+class sf_formatter(dict):
+    # a strict dictionary with a list of _allowed_keys
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args, **kwargs)
+        self._allowed_keys=['shape_functions']        
+        
+    def set_empty_keys(self):
+        for keyname in self._allowed_keys:
+            self.__setitem__(keyname, None)
             
-        pass 
+    def __setitem__(self,key,value):
+        if key in self._allowed_keys:
+            super().__setitem__(key, value)
+        else:
+            raise KeyError((
+                        f"keyname '{key}' is not in list of allowed keys. "
+                        f"Allowed keys are: {','.join(self._allowed_keys)}"
+                        ))
+            
+    def _format_sympy(self,shape_func):
+        return str(shape_func) 
+        
+    def set_shape_functions(self,shape_func_list):
+        """sets the shape_functions key by processing a list of sympy expressions
+
+        Parameters
+        ----------
+        shape_func_list : list 
+            a list of sympy expressions representing the shapefiles
+
+        """
+        
+        if len(shape_func_list) != self.get('num_vertices') and len(shape_func_list)!=0:
+            raise ValueError("length of shape file list must match number of vertices!")
+        
+        shape_functions=[self._format_sympy(sf)+',' for sf in shape_func_list]        
+        self.__setitem__('shape_functions',shape_functions)
+        
+    def get_dict(self):
+        # returns a normal dictionary 
+        return {key : value for key,value in self.items()}
+        
+class sf_yt_formatter(sf_formatter):
+    def __init__(self,mesh_type,num_dim,num_vertices,shape_func_list=[],*args,**kwargs):
+        super().__init__(*args,**kwargs)        
+        self._allowed_keys = ['mesh_type', 'num_dim', 'num_vertices', 
+                           'num_mapped_coords', 'shape_functions']
+        self.set_empty_keys()
+        self.__setitem__('mesh_type',mesh_type)
+        self.__setitem__('num_dim',num_dim)
+        self.__setitem__('mesh_type',mesh_type)
+        self.__setitem__('num_mapped_coords',num_dim)
+        self.__setitem__('num_vertices',num_vertices)
+        self.set_shape_functions(shape_func_list)
+        
+                    
+    def _format_sympy(self,shape_func):
+        # substitution rules, convert to string 
+        x, y, z = sy.symbols('x,y,z')
+    
+        # x,y,z are x[0],x[1],x[2] respectively
+        for c in [[x,0],[y,1],[z,2]]:
+            shape_func = shape_func.replace(c[0],sy.Symbol(f'x[{c[1]}]'))  
+                                
+        return str(shape_func)
+        
+    def set_shape_functions(self,shape_func_list):
+        super().set_shape_functions(shape_func_list)
+        sf_list = self.get('shape_functions')
+        sf_str = '['+'\n'.join(sf_list).rstrip(',') + ']'        
+        self.__setitem__('shape_functions',yaml_support.literal_str(sf_str))
+        
